82b4576ad3ec6e8b9a72245c1bbb192846599c4c
pablolh <p.lehenaff@tudelft.nl>
2023-08-15 12:54:27 +0200

    Edit and process

--- wasm/demo.js
+++ wasm/demo.js
@@ -1,6 +1,22 @@
+session = undefined;
+
 var Module = {
     onRuntimeInitialized: function() {
         session = new Module["SuperpositeurCLISession"]();
         document.getElementById('output').innerHTML = session.input("help");
     }
 };
+
+function input(inputString) {
+    if (session == undefined) {
+        return;
+    }
+
+    newContent = session.input("help");
+
+    document.getElementById('output').innerHTML += "<br> &gt;&gt;&gt; ";
+    document.getElementById('output').innerHTML += inputString;
+    document.getElementById('output').innerHTML += "<br>";
+    document.getElementById('output').innerHTML += newContent;
+}
+  
